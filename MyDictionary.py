1688a239356aea2618c12247864469dcbf3d0c87
Tulsi <tulsithakkar21@gmail.com>
2021-05-24 12:44:40 +0530

    Initial commit

---
+++ MyDictionary.py
@@ -0,0 +1,15 @@
+print("Hello! Welcome to WordBook")
+print("Type a word here :")
+inputWord = (input())
+mydictionary = {"asymptomatic" : "of a condition or a person producing or showing no symptoms",
+                "explore" : "to travel around a place, etc. in order to learn about it",
+                "cherish" : "to keep a thought, feeling, etc. in your mind and think about it often",
+                "ganache" : "a whipped filling of chocolate and cream",
+                "soggy" : "very wet and soft and so unpleasant",
+                "fragile" : "easily damaged or broken",
+                "overwhelm" : "to cause somebody to feel such a strong emotion that he/she does not know how to react",
+                "prejudice" : "a strong unreasonable feeling of not liking or trusting somebody/something",
+                "bland" : "[used about food] mild or lacking in taste",
+                "kudos" : "a word that is used as an expression of approval and praise for a particular achievement"
+                }
+print(mydictionary[inputWord])
